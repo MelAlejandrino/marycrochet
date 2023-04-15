5567b81d7e62d717d5539382076fc25a97b58dbe
MelAlejandrino <125779526+MelAlejandrino@users.noreply.github.com>
2023-04-15 11:47:04 +0800

    mobile buttons

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const buttons = document.querySelectorAll('.buttons');
 window.addEventListener('resize', () => {
   if (window.matchMedia("(min-width: 41em)").matches) {
     buttons.forEach(button => {
-      button.removeAttribute('hidden');
+      // button.removeAttribute('hidden');
     });
   } else {
     buttons.forEach(button => {
-      button.setAttribute('hidden', '');
+      // button.setAttribute('hidden', '');
     });
   }
 });
 
 if (window.matchMedia("(min-width: 41em)").matches) {
     buttons.forEach(button => {
-      button.removeAttribute('hidden');
+      // button.removeAttribute('hidden');
     });
   } else {
     buttons.forEach(button => {
-      button.setAttribute('hidden', '');
+      // button.setAttribute('hidden', '');
     });
   }
